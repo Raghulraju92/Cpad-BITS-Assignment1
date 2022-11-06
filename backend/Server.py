21d0d8d3c1f46afc25f12094cd6f556747f495e0
rru5cob <cmmtg.cob@bcn.bosch.com>
2022-11-06 11:17:00 +0530

    servercode

--- backend/Server.py
+++ backend/Server.py
@@ -1,6 +1,6 @@
 
 from flask import Flask,jsonify,request
-from Database.mysqlhandler import dbhandler
+from mysqlhandler import dbhandler
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,44 +8,65 @@ CORS(app)
 
 db = dbhandler()
 
-@app.route('/getstudentdetails/',methods='POST')
+@app.route('/getstudentdetails/',methods=['GET'])
 def getstudentdetails():
     res = db.getstudentsdata()
     return jsonify(res)
 
 
-@app.route('/getdrivedetails/',methods='POST')
+@app.route('/getdrivedetails/',methods=['GET'])
 def getdrivedetails():
     res = db.getdrivedata()
     return jsonify(res)
 
+@app.route('/getuserdetails/',methods=['POST'])
+def getuserdetails():
+    res = db.getusersdata()
+    return jsonify(res)
 
-@app.route('/updatestudentdetails/',methods='POST')
+@app.route('/updatestudentdetails/',methods=['POST'])
 def updatestudentdetails():
     data = request.get_json()
     res = db.updatestudentsdata(data['student'])
     return jsonify(res)
 
-@app.route('/updatedrivedetails/',methods='POST')
+@app.route('/updatedrivedetails/',methods=['POST'])
 def updatedrivedetails():
     data = request.get_json()
     res = db.updatedrivedata(data['drive'])
     return jsonify(res)
 
-@app.route('/insertstudentdetails/',methods='POST')
+@app.route('/insertstudentdetails/',methods=['POST'])
 def insertstudentdetails():
     data = request.get_json()
     res = db.insertstudentdata(data['students'])
     return jsonify(res)
 
-@app.route('/insertdrivedetails/',methods='POST')
+@app.route('/insertdrivedetails/',methods=['POST'])
 def insertdrivedetails():
     data = request.get_json()
     res = db.insertdrivedata(data['drive'])
-    return jsonify(res)                
+    return jsonify(res)
 
+@app.route('/insertuserdetails/',methods=['POST'])
+def insertuserdetails():
+    data = request.get_json()
+    res = db.insertuserdetails(data['users'])
+    return jsonify(res)
+
+@app.route('/authentication/',methods=['POST'])
+def authentication():
+    data = request.get_json()
+    res = db.authenticate(data['userdetail'])
+    return jsonify(res)
+
+@app.route('/getcounts/',methods=['GET'])
+def getcounts():
+
+    res = db.getcounts()
+    return jsonify(res)
 
 
 if __name__ == '__main__':
  
-    app.run(host='localhost',port=5000)
+    app.run(host='0.0.0.0',port=5000)
--- backend/mysqlhandler.py
+++ backend/mysqlhandler.py
@@ -10,10 +10,10 @@ class dbhandler:
     try:
       self.mydb = mysql.connector.connect(
           host="localhost",
-          user="root",
-          password="Sarababy@2021",
-          port = 3006,
-          database = "student_details"
+          user="cpaduser",
+          password="cpad@2022",
+          port = 3306,
+          database = "vacc_drive"
         )
       print(self.mydb)  
     except:
@@ -24,7 +24,7 @@ class dbhandler:
     try:
 
       mycursor = self.mydb.cursor()
-      sql = "INSERT INTO students (name,date,status) VALUES (%s, %s,%s)"
+      sql = "INSERT INTO students (name,date,status,vacc_name) VALUES (%s, %s,%s,%s)"
       mycursor.execute(sql, val)
       self.mydb.commit()
       res = {"code":200}
@@ -41,7 +41,7 @@ class dbhandler:
     try:
 
       mycursor = self.mydb.cursor()
-      sql = "INSERT INTO drive (name,date_vac,count) VALUES (%s, %s,%s)"
+      sql = "INSERT INTO drive (name,date_vac,count,used_vacc) VALUES (%s, %s,%s,%s)"
       mycursor.execute(sql, val)
       self.mydb.commit()
       res = {"code":200}
@@ -97,7 +97,7 @@ class dbhandler:
     try:
 
        mycursor = self.mydb.cursor()
-       sql = "UPDATE students SET date = %s ,status=%s  WHERE name = %s"
+       sql = "UPDATE students SET date = %s ,status=%s ,vacc_name =%s WHERE name = %s"
        mycursor.execute(sql,val)
        self.mydb.commit()  
        res = {"code":200}
@@ -115,7 +115,7 @@ class dbhandler:
     try:
 
        mycursor = self.mydb.cursor()
-       sql = "UPDATE drive SET date_vac = %s ,count=%s  WHERE name = %s"
+       sql = "UPDATE drive SET date_vac = %s ,count=%s,used_vacc=%s  WHERE name = %s"
        mycursor.execute(sql,val)
        self.mydb.commit() 
        res = {"code":200}
@@ -125,7 +125,51 @@ class dbhandler:
 
        res ={"code":400}
        print(e)
-       return res  
+       return res
+
+  def insertuserdetails(self, val):
+
+      try:
+
+          mycursor = self.mydb.cursor()
+          sql = "INSERT INTO users (name,password) VALUES (%s, %s)"
+          mycursor.execute(sql, val)
+          self.mydb.commit()
+          res = {"code": 200}
+          return res
+
+      except Exception as e:
+
+          res = {"code": 400}
+          print(e)
+          return res
+
+  def authenticate(self,val):
+
+      try:
+          mycursor = self.mydb.cursor()
+          sql = "SELECT * FROM users where name = %s"
+          mycursor.execute(sql,val[0])
+          myresult = mycursor.fetchone()
+          if myresult:
+             password  = myresult[1]
+             testpass = val[1]
+             if password == testpass:
+                res = {"code": 200}
+             else:
+               res = {"code": 201}
+
+             return res
+
+          else:
+              res = {"code": 201}
+              return  res
+
+      except Exception as e:
+
+          res = {"code": 400}
+          print(e)
+          return res
 
 
   def closeconnection(self):
@@ -137,6 +181,66 @@ class dbhandler:
     except Exception as e:
 
        print(e)
+
+  def getusersdata(self):
+
+      try:
+
+          mycursor = self.mydb.cursor()
+          mycursor.execute("SELECT * FROM users")
+          myresult = mycursor.fetchall()
+          out = []
+          for x in myresult:
+              out.append(list(x))
+          res = {"code": 200, "output": out}
+          return res
+
+      except Exception as e:
+
+          res = {"code": 400}
+          print(e)
+          return res
+
+  def getcolumns(self):
+
+      try:
+
+          mycursor = self.mydb.cursor()
+          sql = "SHOW COLUMNS FROM students"
+          out = mycursor.execute(sql)
+          print(out)
+
+      except Exception as e:
+          print(e)
+
+
+  def addcolumn(self):
+
+      try:
+
+          mycursor = self.mydb.cursor()
+          sql = "ALTER TABLE students ADD vacc_name VARCHAR(100)"
+          mycursor.execute(sql)
+          self.mydb.commit()
+
+      except Exception as e:
+          print(e)
+
+
+  def getcounts(self):
+
+      try:
+
+          mycursor = self.mydb.cursor()
+          sql = "select count(*) as total_count,count(if(status='yes',1,null)) as A_count,count(if(status='no',1,null)) as B_count from students"
+          mycursor.execute(sql)
+          myresult = mycursor.fetchall()
+          # print(myresult)
+          res={"code":200,"output":list(myresult[0])}
+          return res
+      except Exception as e:
+          print(e)
+
                
 
 # db = dbhandler()
